0e8c863941011db1622e99183b48886bfc63bcb2
Johan Denoyer <johan.denoyer@gmail.com>
2023-09-08 21:25:23 +0200

    Add PHP repository on Sury.org

--- install-svxlink.sh
+++ install-svxlink.sh
@@ -13,6 +13,11 @@ say () {
 
 MYPATH=${PWD}
 
+say "Installing PHP 8.0 repository"
+run "echo \"deb https://packages.sury.org/php/ $(lsb_release -sc) main\" | sudo tee /etc/apt/sources.list.d/sury-php.list"
+run "wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -"
+run "apt update"
+
 say "Installing SVXLink Prerequisites"
 run "apt install build-essential g++ make cmake libsigc++-2.0-dev php8.0 libgsm1-dev libudev-dev libpopt-dev tcl-dev libgpiod-dev gpiod libgcrypt20-dev libspeex-dev libasound2-dev alsa-utils libjsoncpp-dev libopus-dev rtl-sdr libcurl4-openssl-dev libogg-dev librtlsdr-dev groff doxygen graphviz python3-serial toilet sox -y"
 
